288dee1dbaed1b2c22cd6ac803b8ebf783d95386
M-A-Edwards <markaedw16@gmail.com>
2024-02-10 02:24:19 +0530

    dynamic profile(name and email)

--- lib/pages/ProfileScreen.dart
+++ lib/pages/ProfileScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:legalite/widgets/cdrawer_widget.dart';
@@ -13,120 +14,137 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Profile"),
-      ),
-      drawer: const MyDrawer(),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            height: 250,
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+        appBar: AppBar(
+          title: const Text("Profile"),
+        ),
+        drawer: const MyDrawer(),
+        body: StreamBuilder<DocumentSnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('clients')
+                .doc(FirebaseAuth.instance.currentUser!.uid)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final userData = snapshot.data!.data() as Map<String, dynamic>;
+                return ListView(
                   children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.teal,
-                      minRadius: 65.0,
-                      child: CircleAvatar(
-                        radius: 60.0,
-                        backgroundImage: NetworkImage(
-                            'https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg'),
+                    Container(
+                      height: 250,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              CircleAvatar(
+                                backgroundColor: Colors.teal,
+                                minRadius: 65.0,
+                                child: CircleAvatar(
+                                  radius: 60.0,
+                                  backgroundImage: NetworkImage(
+                                      'https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg'),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                userData['Name'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Client',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Sriharini Margapuri',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      ),
+                    const SizedBox(height: 20),
+                    Column(
+                      children: <Widget>[
+                        const ListTile(
+                          title: Text(
+                            'Date of Birth',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text('08/10/2003'),
+                        ),
+                        const Divider(),
+                        ListTile(
+                          title: Text(
+                            'Email',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(userData['Email']),
+                        ),
+                        const Divider(),
+                        const ListTile(
+                          title: Text(
+                            'Phone Number',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text('+1 310-425-4557'),
+                        ),
+                        const Divider(),
+                        const ListTile(
+                          title: Text(
+                            'Address',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                              '10711 Rose Ave. Apt #112\n90034\n\nLos Angeles, California'),
+                        ),
+                        const Divider(),
+                        ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(50),
+                            ),
+                            icon: const Icon(Icons.lock_open, size: 32),
+                            label: const Text('Sign Out',
+                                style: TextStyle(fontSize: 24)),
+                            onPressed: () => {
+                                  FirebaseAuth.instance.signOut(),
+                                  Navigator.pushReplacementNamed(context, '/'),
+                                  // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                                  //     builder: ((context) => const MainPage())))
+                                }),
+                      ],
                     ),
                   ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Client',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Column(
-            children: <Widget>[
-              const ListTile(
-                title: Text(
-                  'Date of Birth',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text('08/10/2003'),
-              ),
-              const Divider(),
-              const ListTile(
-                title: Text(
-                  'Email',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text('sriharini03.m@gmail.com'),
-              ),
-              const Divider(),
-              const ListTile(
-                title: Text(
-                  'Phone Number',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text('+1 310-425-4557'),
-              ),
-              const Divider(),
-              const ListTile(
-                title: Text(
-                  'Address',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text(
-                    '10711 Rose Ave. Apt #112\n90034\n\nLos Angeles, California'),
-              ),
-              const Divider(),
-              ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(50),
-                  ),
-                  icon: const Icon(Icons.lock_open, size: 32),
-                  label: const Text('Sign Out', style: TextStyle(fontSize: 24)),
-                  onPressed: () => {
-                        FirebaseAuth.instance.signOut(),
-                        Navigator.pushReplacementNamed(context, '/'),
-                        // Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        //     builder: ((context) => const MainPage())))
-                      }),
-            ],
-          ),
-        ],
-      ),
-    );
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }));
   }
 }
