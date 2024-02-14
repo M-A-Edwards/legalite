37c18e920700a4a8aff0a80b7b5fa16c2e53165e
sri-harini-m <sriharini03.m@gmail.com>
2024-02-14 21:39:12 +0530

    homepage

--- lib/Lpages/HomeScreen.dart
+++ lib/Lpages/HomeScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:legalite/widgets/drawer_widget.dart';
+import 'package:legalite/widgets/cdrawer_widget.dart';
 import 'package:legalite/widgets/chatbot.dart';
 
 class Case {
