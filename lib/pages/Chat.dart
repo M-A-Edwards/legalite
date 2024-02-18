5f471dcce112b90aa0773fd89b48eaae844da36a
M-A-Edwards <markaedw16@gmail.com>
2024-02-18 17:37:24 +0530

    just changed the drawer import

--- lib/pages/Chat.dart
+++ lib/pages/Chat.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:legalite/widgets/drawer_widget.dart';
+import 'package:legalite/widgets/cdrawer_widget.dart';
 
 // class ChatPage extends StatefulWidget {
 //   @override
--- lib/pages/ChatPeople.dart
+++ lib/pages/ChatPeople.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:legalite/Lpages/Chat.dart';
-import 'package:legalite/widgets/drawer_widget.dart';
+import 'package:legalite/widgets/cdrawer_widget.dart';
 // import 'package:flutter/rendering.dart';
 // import 'package:legalite/widgets/drawer_widget.dart';
 
