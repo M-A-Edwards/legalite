06daba375f0df920b403f8537c719057cadd5c11
b1gh3ro <saadmujeeb1008@gmail.com>
2024-02-20 20:54:56 +0530

    clients screen with limited clients

--- lib/Lpages/ClientScreen.dart
+++ lib/Lpages/ClientScreen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:legalite/widgets/drawer_widget.dart';
 
@@ -10,44 +11,129 @@ class Clients extends StatefulWidget {
 }
 
 class _ClientsState extends State<Clients> {
+  late TextEditingController _searchController;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  User? _user;
+  List<DocumentSnapshot> chatUsers = [];
+
   @override
+  void initState() {
+    super.initState();
+    _user = _auth.currentUser;
+
+    _searchController = TextEditingController();
+    fetchChatData();
+  }
+
+  void fetchChatData() async {
+    final messagesref = _firestore.collection("messages");
+
+    final messagesRSnapshot =
+        await messagesref.where("recipientId", isEqualTo: _user!.uid).get();
+    final messagesSSnapshot =
+        await messagesref.where("senderId", isEqualTo: _user!.uid).get();
+    List allmessages = [...messagesRSnapshot.docs, ...messagesSSnapshot.docs];
+
+    Set<String> chatUserIds = <String>{};
+    allmessages.forEach((messageDoc) {
+      Map<String, dynamic> messageData =
+          messageDoc.data() as Map<String, dynamic>;
+      String senderId = messageData['senderId'];
+      String recipientId = messageData['recipientId'];
+      debugPrint("sender id: ${senderId} reciepeintId: ${recipientId}");
+      if (senderId != _user!.uid) {
+        chatUserIds.add(senderId);
+      }
+      if (recipientId != _user!.uid) {
+        chatUserIds.add(recipientId);
+      }
+    });
+
+    final profilesCollection = _firestore.collection("clients");
+    for (String userId in chatUserIds) {
+      debugPrint("userId: $userId");
+      DocumentSnapshot userSnapshot =
+          await profilesCollection.doc(userId).get();
+      if (userSnapshot.exists) {
+        debugPrint("id found it is: ${userSnapshot.id}");
+        chatUsers.add(userSnapshot);
+      } else {
+        debugPrint("shiz not exist onli naaaa");
+      }
+    }
+    setState(() {
+      // Update the state variables here if needed
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("clients"),
-        ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection('clients').snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            List<DocumentSnapshot> client = snapshot.data!.docs;
-            return ListView.builder(
-              itemCount: client.length,
-              itemBuilder: (context, index) {
-                String clientId = client[index].id;
-                Map<String, dynamic> clientData =
-                    client[index].data() as Map<String, dynamic>;
-                debugPrint('client data: $clientData');
-                String clientName = clientData['Name'];
-                return ListTile(
-                  title: Text(clientName),
-                  subtitle: Text('Hyderabad'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => clientDetails(
-                              clientName: clientName, clientId: clientId)),
+    List<DocumentSnapshot> filteredClients = chatUsers.where((client) {
+      String clientName = client['Name'].toString().toLowerCase();
+      String searchTerm = _searchController.text.toLowerCase();
+      return clientName.contains(searchTerm);
+    }).toList();
+
+    if (chatUsers.isEmpty || chatUsers == []) {
+      return Scaffold(
+          drawer: const MyDrawer(),
+          appBar: AppBar(
+            title: const Text("My Clients"),
+          ),
+          body: const Center(
+            child: CircularProgressIndicator(),
+          ));
+    } else {
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text("My Clients"),
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: const InputDecoration(
+                    labelText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: filteredClients.length,
+                  itemBuilder: (context, index) {
+                    String clientId = filteredClients[index].id;
+                    Map<String, dynamic> clientData =
+                        filteredClients[index].data() as Map<String, dynamic>;
+                    String clientName = clientData['Name'];
+
+                    return ListTile(
+                      title: Text(clientName),
+                      subtitle: Text('Hyderabad'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => clientDetails(
+                              clientName: clientName,
+                              clientId: clientId,
+                            ),
+                          ),
+                        );
+                      },
                     );
                   },
-                );
-              },
-            );
-          },
-        ),
-        drawer: const MyDrawer());
+                ),
+              ),
+            ],
+          ),
+          drawer: const MyDrawer());
+    }
   }
 }
 
@@ -144,7 +230,6 @@ class clientDetails extends StatelessWidget {
           ],
         ),
       ),
-      drawer: const MyDrawer(),
     );
   }
 }
